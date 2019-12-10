5105261f4a583b34a6bad4139b3819fd2028a3f3
Santhosh Krishnan <santhosh.krishnan007@gmail.com>
2019-12-10 14:41:43 +0530

    Update botos3.py

--- ansible/botos3.py
+++ ansible/botos3.py
@@ -1,3 +1,11 @@
 import boto3
-s3 = boto3.client('s3',region_name='ap-southeast-1')
-s3.create_bucket(Bucket='my-bucket-qwqw007sant0007us')
+#s3 = boto3.client('s3',region_name='ap-southeast-1')
+#s3.create_bucket(Bucket='my-bucket-qwqw007sant0007us')
+#import boto3
+ec2 = boto3.resource('ec2')
+# create VPC
+vpc = ec2.create_vpc(CidrBlock='172.16.0.0/16')
+# assign a name to our VPC
+vpc.create_tags(Tags=[{"Key": "Name", "Value": "my_vpc"}])
+vpc.wait_until_available()
+
