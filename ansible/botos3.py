9fa38034f181378094845de1ea1b86cb63d2fb83
Santhosh Krishnan <santhosh.krishnan007@gmail.com>
2019-12-10 15:00:55 +0530

    Update botos3.py

--- ansible/botos3.py
+++ ansible/botos3.py
@@ -4,7 +4,7 @@ import boto3
 #import boto3
 ec2 = boto3.resource('ec2')
 # create VPC
-vpc = ec2.create_vpc(CidrBlock='172.16.0.0/16')
+vpc = ec2.create_vpc(CidrBlock='172.16.0.0/16',region_name='ap-southeast-1')
 # assign a name to our VPC
 vpc.create_tags(Tags=[{"Key": "Name", "Value": "my_vpc"}])
 vpc.wait_until_available()
