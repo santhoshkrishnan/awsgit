99ee40701fe5cd62a5e37843ca5a2e748cc646ec
Santhosh Krishnan <santhosh.krishnan007@gmail.com>
2019-12-12 21:50:54 +0530

    Update vpc.py

--- ansible/vpc.py
+++ ansible/vpc.py
@@ -6,5 +6,5 @@ ec2 = boto3.resource('ec2',region_name='us-east-1')
 # create VPC
 vpc = ec2.create_vpc(CidrBlock='172.31.0.0/16')
 #assign a name to our VPC
-vpc.create_tags(Tags=[{"Key": "Name", "Value": "my_vpc3"}])
+vpc.create_tags(Tags=[{"Key": "Name", "Value": "my_vpc4"}])
 vpc.wait_until_available()#
