af5f9eb1b0724e282841b1ae3c15f1b65bca3a7b
Santhosh Krishnan <santhosh.krishnan007@gmail.com>
2019-12-12 14:30:19 +0530

    Update vpc.py

--- ansible/vpc.py
+++ ansible/vpc.py
@@ -2,9 +2,9 @@ import boto3
 #s3 = boto3.client('s3',region_name='us-east-1')
 #s3.create_bucket(Bucket='my-bucket-sssssseee45')
 #import boto3
-#ec2 = boto3.resource('ec2',region_name='us-east-1')
+ec2 = boto3.resource('ec2',region_name='us-east-1')
 # create VPC
-#vpc = ec2.create_vpc(CidrBlock='172.30.0.0/16')
-# assign a name to our VPC
-#vpc.create_tags(Tags=[{"Key": "Name", "Value": "my_vpc3"}])
-#vpc.wait_until_available()#
+vpc = ec2.create_vpc(CidrBlock='172.31.0.0/16')
+#assign a name to our VPC
+vpc.create_tags(Tags=[{"Key": "Name", "Value": "my_vpc3"}])
+vpc.wait_until_available()#
