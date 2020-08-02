8d6e6a9288f5c2729e7537290203454d88bdc49e
Santhosh Krishnan <santhosh.krishnan007@gmail.com>
2020-08-02 17:48:56 +0530

    Update botos3.py

--- ansible/botos3.py
+++ ansible/botos3.py
@@ -1,6 +1,6 @@
 import boto3
-#s3 = boto3.client('s3',region_name='us-east-1')
-s3 = boto3.client('s3')
+s3 = boto3.client('s3',region_name='us-east-1')
+#s3 = boto3.client('s3')
 s3.create_bucket(Bucket='my-bucket-sssssseee455555555555555')
 #import boto3
 #ec2 = boto3.resource('ec2',region_name='us-east-1')
